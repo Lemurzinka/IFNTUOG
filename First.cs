27cb78488bdb768291745a147dbfbe00223f00bd
Lemurzinka <154348981+Lemurzinka@users.noreply.github.com>
2024-09-26 18:08:22 +0300

    Add files via upload

---
+++ First.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class First
+{
+    static void Main()
+    {
+       
+        Console.WriteLine("Обчислення відстані між населеними пунктами.");
+        Console.Write("Масштаб карти (кількість кілометрів в одному сантиметрі) -> ");
+        double scale = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Відстань між точками, які позначають населені пункти (См) -> ");
+        double distanceInCm = Convert.ToDouble(Console.ReadLine());
+
+       
+        double distanceInKm = scale * distanceInCm;
+
+      
+        Console.WriteLine($"Відстань між населеними пунктами {distanceInKm} км.");
+    }
+}
