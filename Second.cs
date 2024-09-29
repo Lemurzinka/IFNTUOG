f2d483db1d4ff4bb04d2f4bc77667bd57b2f01a0
Lemurzinka <154348981+Lemurzinka@users.noreply.github.com>
2024-09-29 18:01:19 +0300

    Add files via upload

---
+++ Second.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Second
+{
+    static void Main()
+    {
+        
+        Console.Write("Введiть рiк, наприклад 2000, -> ");
+        int year = Convert.ToInt32(Console.ReadLine());
+
+        
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        {
+            Console.WriteLine($"{year} рiк - високосний");
+        }
+        else
+        {
+            Console.WriteLine($"{year} рiк - не високосний");
+        }
+    }
+}
