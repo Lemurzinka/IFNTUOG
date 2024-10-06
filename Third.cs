03c33a6aba4778ac1cf53b2c205d73d114e8c090
Lemurzinka <154348981+Lemurzinka@users.noreply.github.com>
2024-10-06 15:28:46 +0300

    Add files via upload

---
+++ Third.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace gevkach
+{
+    internal class Third
+    {
+        static void Main() {
+            int[] array = {1, 3, 5, 5, 3};
+
+            Console.Write("Enter a number to search: ");
+            int numberToSearch = int.Parse(Console.ReadLine());
+
+            int calculatedAppears = CalculateNumberOfMeeting(numberToSearch, array);
+
+            Console.Write($"The number {numberToSearch} appears {calculatedAppears} times in array");
+            
+
+        }
+
+        static int CalculateNumberOfMeeting(int number, int[] array ) 
+        {
+            int counter = 0;
+            foreach(int element in array){
+                if (element == number) {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+    }
+}
