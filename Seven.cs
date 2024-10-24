702dff70d15d767c51e2b0e910ef546d291f4fa9
Lemurzinka <154348981+Lemurzinka@users.noreply.github.com>
2024-10-24 12:03:17 +0300

    Add files via upload

---
+++ Seven.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Seven
+{
+    static void Main()
+    {
+        string directoryPath = Directory.GetCurrentDirectory(); //ну або його можемо і вказати :)
+        Stack<string> directoryStack = new Stack<string>();
+
+        try
+        {
+            foreach (var file in Directory.GetFiles(directoryPath))
+            {
+                directoryStack.Push(file);
+            }
+
+            foreach (var dir in Directory.GetDirectories(directoryPath))
+            {
+                directoryStack.Push(dir);
+            }
+
+            Console.WriteLine("Elements in stack:");
+            while (directoryStack.Count > 0)
+            {
+                Console.WriteLine(directoryStack.Pop());
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
