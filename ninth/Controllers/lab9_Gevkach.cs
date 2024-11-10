06cab6ca80b014a31c3ef8ad14c477343a282711
Lemurzinka <154348981+Lemurzinka@users.noreply.github.com>
2024-11-10 16:04:04 +0200

    Add files via upload

---
+++ ninth/Controllers/lab9_Gevkach.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class lab9_GevkachController : Controller
+    {
+       //GET: lab9_Gevkach
+        public ActionResult FirstViewMethod()
+        {
+            List<string> autos =  GetAutoList();
+            return View(autos);
+        }
+        public ActionResult SecondViewMethod() 
+        { 
+        return View(GetAutoList().OrderBy(x=>x).ToList());
+        }
+        public ActionResult ThirdViewMethod()
+        {
+            return View(GetAutoList().OrderBy(x=>x).ToList());
+        }
+
+        public List<string> GetAutoList()
+        {
+            List<string> autos = new List<string>();
+            autos.Add("BMW");
+            autos.Add("Tesla");
+            autos.Add("Mazda");
+            autos.Add("Alfa Romeo");
+            autos.Add("Maserati");
+            autos.Add("Ferrari");
+            autos.Add("Porsche");
+
+            return autos;
+        }
+    }
+}
