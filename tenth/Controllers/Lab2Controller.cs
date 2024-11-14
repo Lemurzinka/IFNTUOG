bab2e27a196ad32e929197e8a010764e6d155e02
Lemurzinka <154348981+Lemurzinka@users.noreply.github.com>
2024-11-14 17:08:40 +0200

    Add files via upload

---
+++ tenth/Controllers/Lab2Controller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models.Entities;
+
+namespace WebApplication1.Controllers
+{
+    public class Lab2Controller : Controller
+    {
+        // GET: Lab2
+      
+        public ActionResult ListOfPeople()
+        {
+            List<Person> people = new List<Person>();
+            using (var db = new Model1Enities()) 
+            {
+
+                people = db.people.OrderByDescending(x => x.Age)
+                                  .ThenBy(x => x.LastName)
+                                  .ThenBy(x => x.FirstName).ToList();
+            }
+            return View(people); 
+        }
+
+        public ActionResult PersonDetails(Guid personId)
+        {
+            Person model = new Person();
+            using (var db = new Model1Enities())
+            {
+
+                model = db.People.Find(personId);
+                                 
+            }
+            return View(mode1);
+        }
+    }
+}
---
+++ tenth/Controllers/Model1Enities.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using WebApplication1.Models.Entities;
+
+namespace WebApplication1.Controllers
+{
+    internal class Model1Enities : IDisposable
+
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public partial class Person
+        {
+            [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+            public Person()
+            {
+                this.Answers = new HashSet<Answer>();
+            }
+
+            public System.Guid Id { get; set; }
+            public string LastName { get; set; }
+            public string FirstName { get; set; }
+            public string Patronymic { get; set; }
+            public int Age { get; set; }
+            public string Gender { get; set; }
+            public bool HasJob { get; set; }
+
+            [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+            public virtual ICollection<Answer> Answers { get; set; }
+        }
+    }
+}
